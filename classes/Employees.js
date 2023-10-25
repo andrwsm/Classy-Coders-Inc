0a643febf99cb9f82078595cf9a878bf8f7bcd45
Andrew Smith <andrew.smith1@hl.co.uk>
2023-10-25 12:58:12 +0100

    attempt2

--- classes/Employees.js
+++ classes/Employees.js
@@ -1,6 +1,33 @@
 class Employees {
+#salary
+#isHired
+
+constructor(name, position, salary, isHired){
+    this.name = name
+    this.position = position
+    this.#salary = salary
+    this.#isHired = isHired
+}
+
+getSalary(){
+    return this.#salary
+}
+getStatus(){
+    return this.#isHired
+}
+setStatus(command){
+    if(command == 'hire'){
+        this.#isHired = true
+    } else if (command == 'fire'){
+       this.#isHired = false
+    }
+}
+setSalary(amount){
+    this.#salary = amount
+}
 
 
+//  andrwsm
 
     
 }
